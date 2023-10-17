8b4587046888cf1996e5ab065176f3ca8d048499
korberlin <gorkemm.koksal@gmail.com>
2023-10-17 17:16:11 +0200

    styling and README.md

---
+++ README.md
@@ -0,0 +1,38 @@
+
+
+# Etch-a-Sketch Project
+
+![Etch-a-sketch Preview](/img/etch-a-sketch.png) 
+
+> Replace `path_to_your_preview_image.png` with an actual path if you have a preview image of your project.
+
+## Description
+
+This project simulates the classic Etch-a-Sketch toy, allowing users to draw on a grid by simply hovering the mouse over the cells. Users can choose between a traditional black pen or a colorful rainbow pen to make their drawings even more vibrant!
+
+## Features
+
+- **Dynamic Grid Size**: Users can change the grid size between 16x16 and 100x100.
+- **Rainbow and Black Pen Modes**: Toggle between a colorful rainbow pen or a classic black pen.
+- **Responsive Grid**: Regardless of the number of cells, the grid retains its size, only changing the individual cell dimensions.
+- **Reset Button**: Easily reset your artwork and start anew with a clean slate.
+
+## Usage
+
+1. **Drawing**: Hover over the grid cells to start drawing.
+2. **Change Grid Size**: Click the "Change the grid size" button and enter a number between 16 and 100.
+3. **Resetting the Board**: Click the "Reset the board" button to clear the grid and set it back to its default size.
+4. **Toggle Pen Mode**: Use the "Pen: Black" or "Pen: Rainbow" button to toggle between the two pen modes.
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- Vanilla JavaScript
+
+## Future Improvements
+
+- Mobile responsiveness.
+- Ability to save and load drawings.
+- Additional pen colors and effects.
+
--- js-file.js
+++ js-file.js
@@ -1,81 +1,68 @@
 const gridContainer = document.querySelector("#gridContainer");
-const body = document.querySelector("body");
+const options = document.querySelector("#options");
+
+let isRainbow = false;
 
 function generateGrid(size) {
-    let oldGrids = gridContainer.querySelectorAll(".gridRow");
-    oldGrids.forEach(Element => {
-        gridContainer.removeChild(Element);
-    });
+    const fragment = document.createDocumentFragment();
+
     for (let i = 0; i < size; i++) {
         const gridRow = document.createElement("div");
-        gridRow.className = "gridRow"
+        gridRow.className = "gridRow";
         for (let j = 0; j < size; j++) {
             const gridCell = document.createElement("div");
             gridCell.className = "gridCell";
             gridCell.addEventListener("mouseover", function () {
-
-                color = getPenColor(isRainbow);
-                gridCell.style.backgroundColor = color;
-            })
+                gridCell.style.backgroundColor = getPenColor(isRainbow);
+            });
             gridRow.appendChild(gridCell);
         }
-        gridContainer.appendChild(gridRow);
+        fragment.appendChild(gridRow);
     }
+
+    gridContainer.innerHTML = '';
+    gridContainer.appendChild(fragment);
 }
+
+function getPenColor(isRainbow) {
+    if (isRainbow) {
+        const red = Math.floor(Math.random() * 256);
+        const green = Math.floor(Math.random() * 256);
+        const blue = Math.floor(Math.random() * 256);
+        return `rgb(${red}, ${green}, ${blue})`;
+    }
+    return "black";
+}
+
+function getToggleName(isRainbow) {
+    return isRainbow ? "Pen: Rainbow" : "Pen: Black";
+}
+
+// Initialize Grid and Buttons
 generateGrid(16);
 
 const gridButton = document.createElement("button");
 gridButton.textContent = "Change the grid size";
-
-function getprompt() {
-    let userInput = prompt("Enter a number between 16 and 100.");
-    let parseInput = parseInt(userInput);
+gridButton.addEventListener("click", function () {
+    const userInput = prompt("Enter a number between 16 and 100.");
+    const parseInput = parseInt(userInput);
     if (!isNaN(parseInput) && parseInput >= 16 && parseInput <= 100) {
         generateGrid(parseInput);
     } else {
         alert("You entered an invalid option.");
     }
-}
-
-gridButton.addEventListener("click", getprompt);
-const options = document.querySelector("#options");
+});
 options.appendChild(gridButton);
+
 const resetButton = document.createElement("button");
 resetButton.textContent = "Reset the board.";
-resetButton.addEventListener("click", function () {
-    generateGrid(16);
-})
+resetButton.addEventListener("click", () => generateGrid(16));
 options.appendChild(resetButton);
-let isRainbow = false;
+
 const togglePen = document.createElement("button");
-togglePen.id = "togglePen";
+togglePen.textContent = getToggleName(isRainbow);
 togglePen.addEventListener("click", function () {
     isRainbow = !isRainbow;
-    togglePen.textContent = getToggleName(isRainbow)
-})
-togglePen.textContent = getToggleName(isRainbow);
+    togglePen.textContent = getToggleName(isRainbow);
+});
 options.appendChild(togglePen);
-
-function getToggleName(isRainbow) {
-    let name = "Pen: Black";
-    if (isRainbow) {
-        name = "Pen: Rainbow";
-        return name;
-    }
-    return name;
-}
-
-
-function getPenColor(isRainbow) {
-    let color = "black";
-    if (isRainbow === true) {
-        let red = Math.floor(Math.random() * 256);
-        let blue = Math.floor(Math.random() * 256);
-        let green = Math.floor(Math.random() * 256);
-        color = `rgb(${red}, ${green}, ${blue})`;
-        return color;
-    }
-
-    return color;
-
-}
